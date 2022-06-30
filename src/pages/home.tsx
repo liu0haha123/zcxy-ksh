5e5b008907239b2ea8ebad330cd4aa865b65aad7
lw <1109160884@qq.com>
2022-06-30 10:38:40 +0800

    使用gird实现布局

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './home.scss';
-import headerBg from "../images/header.png"
+import React from "react";
+import "./home.scss";
+import headerBg from "../images/header.png";
 
 export const Home = () => {
   return (
-    <div className='home'>
-      <div className='header' style={{backgroundImage:`url(${headerBg})`}}>
-
-      </div>
+    <div className="home">
+      <header
+        className="header"
+        style={{ backgroundImage: `url(${headerBg})` }}
+      ></header>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+        <section className="section4"></section>
+        <section className="section5"></section>
+      </main>
     </div>
   );
 };
