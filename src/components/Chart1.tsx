1a84c5d744b6d1cb836e1eebbbef141c13f9f01e
lw <1109160884@qq.com>
2022-07-01 09:06:47 +0800

    抽取了echarts的通用初始化设置

--- src/components/Chart1.tsx
+++ src/components/Chart1.tsx
@@ -1,73 +1,68 @@
 import React, { useEffect, useRef } from "react";
 import * as echarts from "echarts";
+import { px } from "../shared/px";
+import { createEchartsOptions } from "../shared/create-echarts-options";
+import { baseEchartOptions } from "../shared/base-echart-options";
 
-const px = (n) => (n / 2420) * (window as any).pageWidth;
+
 
 export const Chart1 = () => {
   const divRef = useRef(null);
   useEffect(() => {
     var myChart = echarts.init(divRef.current);
-    myChart.setOption({
-      textStyle: {
-        fontSize: px(12),
-        color: "#79839E",
-      },
-      title: { show: false },
-      legend: { show: false },
-      xAxis: {
-        data: [
-          "医疗保险",
-          "工伤保险",
-          "养老保险",
-          "失业保险",
-          "生育保险",
-          "住房公积金",
-        ],
-        axisTick: { show: false },
-        axisLine: {
-          lineStyle: { color: "#083B70" },
-        },
-        axisLabel: {
-          fontSize: px(12),
-          formatter(val) {
-            if (val.length > 2) {
-              const array = val.split("");
-              array.splice(2, 0, "\n");
-              return array.join("");
-            } else {
-              return val;
-            }
+    myChart.setOption(
+      createEchartsOptions({
+        ...baseEchartOptions,
+        xAxis: {
+          data: [
+            "医疗保险",
+            "工伤保险",
+            "养老保险",
+            "失业保险",
+            "生育保险",
+            "住房公积金",
+          ],
+          axisTick: { show: false },
+          axisLine: {
+            lineStyle: { color: "#083B70" },
+          },
+          axisLabel: {
+            fontSize: px(12),
+            formatter(val) {
+              if (val.length > 2) {
+                const array = val.split("");
+                array.splice(2, 0, "\n");
+                return array.join("");
+              } else {
+                return val;
+              }
+            },
           },
         },
-      },
-      grid: {
-        x: px(40),
-        y: px(40),
-        x2: px(40),
-        y2: px(40),
-      },
-      yAxis: {
-        splitLine: { show: false },
-        axisLine: {
-          show: true,
-          lineStyle: { color: "#083B70" },
+
+        yAxis: {
+          splitLine: { show: false },
+          axisLine: {
+            show: true,
+            lineStyle: { color: "#083B70" },
+          },
+          axisLabel: {
+            fontSize: px(12),
+          },
         },
-        axisLabel: {
-          fontSize: px(12),
-        },
-      },
-      series: [
-        {
-          type: "bar",
-          data: [10, 20, 36, 41, 15, 26],
-        },
-      ],
-    });
+        series: [
+          {
+            type: "bar",
+            data: [14, 40, 36, 41, 15, 36],
+          },
+        ],
+      })
+    );
   }, []);
 
   return (
     <div className="bordered 管辖统计">
-      <h2>近日发布政策条目数</h2>
+      <h2>政策条目数</h2>
       <div ref={divRef} className="chart"></div>
     </div>
   );
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,8 +3,8 @@ import Clock from "../components/Clock";
 import "./home.scss";
 import "@icon-park/react/styles/index.css";
 import { Bill } from "@icon-park/react";
-import * as echarts from "echarts";
 import { Chart1 } from "../components/Chart1";
+import { Chart2 } from "../components/Chart2";
 
 export const Home = () => {
   return (
@@ -12,12 +12,12 @@ export const Home = () => {
       <header>
         <Clock />
         <span className="title">政策血缘数据可视化展示</span>
-
-        <Bill theme="outline" size="24" fill="#6d929d" strokeLinejoin="bevel" />
+        <Bill theme="outline" size="32" fill="#6d929d" strokeLinejoin="bevel" />
       </header>
       <main>
         <section className="section1">
           <Chart1 />
+          <Chart2 />
         </section>
         <section className="bordered section2"></section>
         <section className="bordered section3"></section>
